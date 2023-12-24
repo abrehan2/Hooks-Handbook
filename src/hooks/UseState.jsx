5b0480b1da79e2297c16ee0ab704db77825fcb04
abrehan2 <rehannajam2@gmail.com>
2023-12-24 19:59:12 +0500

    EXAMPLE 3: Capture and display form input with useState()

--- src/hooks/UseState.jsx
+++ src/hooks/UseState.jsx
@@ -20,30 +20,80 @@ const UseState = () => {
   //       </div>
   //     </>
   //   );
-
   // EXAMPLE 2: Render array of objects with useState() and Map function
+  //   const initialState = [
+  //     {
+  //       name: "Abdul Rehan",
+  //       work: "Full-Stack Engineer",
+  //     },
+  //   ];
+  //   const [value, setValue] = useState(initialState);
+  //   return (
+  //     <>
+  //       <div className="__main__">
+  //         {value &&
+  //           value.map((val, index) => {
+  //             return (
+  //               <ul className="__list__" key={index}>
+  //                 <li>Name: {val.name}</li>
+  //                 <li>Work: {val.work}</li>
+  //               </ul>
+  //             );
+  //           })}
+  //       </div>
+  //     </>
+  //   );
+  // EXAMPLE 3: Capture and display form input with useState()
 
-  const initialState = [
-    {
-      name: "Abdul Rehan",
-      work: "Full-Stack Engineer",
-    },
-  ];
+  const [value, setValue] = useState({
+    name: "",
+    age: "",
+  });
 
-  const [value, setValue] = useState(initialState);
+  const [result, setResult] = useState(null);
+
+  const getInput = (e) => {
+    e.preventDefault();
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
+  const getResult = (e) => {
+    e.preventDefault();
+    setResult(value);
+  };
 
   return (
     <>
-      <div className="__main__">
-        {value &&
-          value.map((val, index) => {
-            return (
-              <ul className="__list__" key={index}>
-                <li>Name: {val.name}</li>
-                <li>Work: {val.work}</li>
-              </ul>
-            );
-          })}
+      <div className="__form__main__">
+        <form onSubmit={getResult}>
+          <input
+            type="text"
+            placeholder="Name"
+            className="__input__"
+            required
+            name="name"
+            onChange={getInput}
+          />
+          <input
+            type="number"
+            placeholder="Age"
+            className="__input__"
+            required
+            name="age"
+            onChange={getInput}
+          />
+
+          <div className="__btn__">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+
+        {result && (
+          <div className="__result__">
+            <p>Name: {result?.name}</p>
+            <p>Age: {result?.age}</p>
+          </div>
+        )}
       </div>
     </>
   );
